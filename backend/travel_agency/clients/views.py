d6acf0cf43d48d8afdf2cde1a1a78e483dd179d8
tpons <tponsmiquel@gmail.com>
2025-11-10 19:06:57 +0100

    feat: add client view

--- backend/travel_agency/clients/views.py
+++ backend/travel_agency/clients/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Client
+from .serializers import ClientSerializer
 
 # Create your views here.
+class ClientViewSet(viewsets.ModelViewSet):
+    """API REST for Client management.
+    Allows listing, creating, updating, and deleting records.
+    """
+    queryset = Client.objects.all().order_by('id')
+    serializer_class = ClientSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['first_name', 'last_name', 'email', 'document_number']
+    ordering_fields = ['id', 'last_name', 'created_at']
