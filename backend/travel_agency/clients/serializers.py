e18419036ac6f6c369fae4a95e3f6b5175a5f53d
tpons <tponsmiquel@gmail.com>
2025-11-10 18:54:31 +0100

    feat: add client serializer

---
+++ backend/travel_agency/clients/serializers.py
@@ -0,0 +1,15 @@
+from backend.travel_agency.clients.models import Client
+from rest_framework import serializers
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = [
+            'id', 'client_type', 'first_name', 'last_name',
+            'document_type', 'document_number', 
+            'email', 'phone_mobile', 'phone_landline',
+            'address', 'city', 'postal_code', 'province', 'country',
+            'is_active', 'created_at', 'updated_at'
+        ]
+        read_only_fields = ['id', 'created_at', 'updated_at']
