5349a9e8bd323fb75de851327995a528c794d35b
tpons <tponsmiquel@gmail.com>
2025-11-13 23:10:23 +0100

    feat: add service admin

--- backend/travel_agency/services/admin.py
+++ backend/travel_agency/services/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Service
 
-# Register your models here.
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'service_type', 'dossier', 'supplier',
+        'start_date', 'origin', 'destination',
+        'total_client', 'total_supplier',
+        'created_at'
+    )
+    search_fields = (
+        'service_type',
+        'dossier__code',
+        'supplier__name',
+        'origin',
+        'destination'
+    )
+    list_filter = ('service_type', 'is_active')
+    ordering = ('start_date',)
