7cee78115ed1f8400cd2c976e89b6edea784bc16
tpons <tponsmiquel@gmail.com>
2025-11-13 18:23:41 +0100

    feat: register dossier model in admin

--- backend/travel_agency/dossiers/admin.py
+++ backend/travel_agency/dossiers/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Dossier
 
 # Register your models here.
+@admin.register(Dossier)
+class DossierAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'code', 'title', 'status', 'client', 'start_date', 'end_date', 
+        'total_pvp', 'total_paid', 'total_pending', 'total_invoiced', 'created_at'
+        )
+    search_fields = ('code', 'title', 'client__first_name', 'client__last_name')
+    list_filter = ('status', 'is_active')
+    ordering = ('-created_at',)
